e2dd84bcd2378f8e65d0fc3762c6dbba79ff7e38
965310001@qq.com <965310001@qq.com>
2019-03-22 16:09:31 +0800

    组件化的初步完成

---
+++ module_me/src/main/java/com/mingpinmall/me/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.mingpinmall.me;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @Author: guofeng
+ * @CreateDate:
+ * @Description: 登录
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
